daf246fdb7b4027e8016daf825ebaebe97cf5d05
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-10 17:39:04 +0530

    Initial commit

---
+++ Binary_search using functions.py
@@ -0,0 +1,21 @@
+def Binary_search(l,size,k):
+    low=0
+    high=size-1
+    while(low<=high):
+       mid=(low+high)//2
+       if l[mid]==k:
+         return mid
+       elif l[mid]<k:
+         low=mid+1 
+       else:
+         high=mid-1
+    return -1
+size=int(input("Enter the size of the array:"))
+print("Enter array elements in sorted manner: ")
+l=list(map(int,input().split()))
+k=int(input("Enter the element to be searched in given array: "))
+idx=Binary_search(l,size,k)
+if idx!=-1:
+    print("The element is found at index: ",idx)
+else:
+    print("Element ",k," is not found in the given array.")
