43b0b189492f41f2bcc845c78d45925d6c3e5343
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-10 17:38:40 +0530

    Initial commit

---
+++ Linearch_search using functions.py
@@ -0,0 +1,15 @@
+def linear_search(l,size,k):
+      for i in range(size):
+        if l[i]==k:
+           return i 
+      return -1
+ 
+size=int(input("Enter the size of the array:"))
+print("Enter array elements: ")
+l=list(map(int,input().split()))
+k=int(input("Enter the element to be searched in given array: "))
+idx=linear_search(l,size,k)
+if idx!=-1:
+ print("The element is present at index: ",idx)
+else:
+    print("Element ",k," is not found in the given array.")
