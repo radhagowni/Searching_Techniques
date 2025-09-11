78bd910090cf023510bfb308545074eeb5b54f10
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-09-11 17:39:05 +0530

    Initial commit

---
+++ Binary_search_ceil.py
@@ -0,0 +1,20 @@
+s=int(input())
+l=list(map(int,input().split()))
+element=int(input())
+low=0
+ans=-1
+high=s-1
+while(low<=high):
+    mid=(low+high)//2
+    if l[mid]==element:
+        ans=mid
+        break
+    elif (l[mid]>element):
+        ans=mid
+        high=mid-1 
+    else:
+        low=mid+1
+if ans==-1:
+    print("No such element is found and there is no ceil value of ",element," in the given array")
+else:
+   print("The ceil value or element ",element," is found at index ",ans)
